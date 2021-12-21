36d6afe8dbbba4567a0ac02a23383f8c9e1dd94c
pfeiff190100 <pfeiff190100@sr.htlweiz.at>
2021-12-21 12:35:45 +0100

    added full_lengh to view the full lengh moved Diagonaly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, pyautogui, time
+import sys, pyautogui, time, math
 from threading import Thread
 from PyQt5.QtWidgets import QApplication
 
@@ -7,10 +7,12 @@ class movlen:
     mouse_events = []
     x_nums = []
     y_nums = []
-    fulllengh = []
+    fulllengh_nums = []
     pixelsize = 0
     cmx = 0
     cmy = 0    
+    full_len = 0
+    
 
     def __init__(self) -> None:
         pass
@@ -26,18 +28,20 @@ class movlen:
         lastposy = 0
         while True:
             pos = pyautogui.position()
-            if(pos.x != lastposx):
+            if(pos.x != lastposx or pos.y != lastposy):
                 #calclulates the diffrent between two yx numbers
                 if lastposx == 0:
                     lastposx = pos.x
-                self.x_nums.append(pos.x - lastposx)
-                self.fulllengh.append
-                lastposx = pos.x
-            if pos.y != lastposy:
                 #calclulates the diffrent between two y numbers
                 if lastposy == 0:
                     lastposy = pos.y
+                #saves the lengh moved to a array
+                self.x_nums.append(pos.x - lastposx)                    
                 self.y_nums.append(pos.y - lastposy)
+                #saves the Diagonal of the x and y value to an array
+                self.fulllengh_nums.append(math.sqrt((pos.x - lastposx)  * (pos.x - lastposx)  + (pos.y - lastposy) * (pos.y - lastposy)))
+                #change last position of x and y
+                lastposx = pos.x                
                 lastposy = pos.y
             time.sleep(0.01)
             
@@ -46,10 +50,9 @@ class movlen:
             i = input()
             if i == "mov":
                 self.savedata()
-                print(self.convert(self.cmx * self.pixelsize, "x:"))
-                print(self.convert(self.cmy * self.pixelsize, "y:"))
-                self.x_nums.clear()
-                self.y_nums.clear()
+                print(self.convert(self.full_len * self.pixelsize, "full lengh moved on screen:"))
+                print(self.convert(self.cmx * self.pixelsize, "lengh moved on x Axes:"))
+                print(self.convert(self.cmy * self.pixelsize, "lengh moved on y Axes:"))
             elif i == "screensize":
                 print(self.getscreensize())
             elif i == "help":
@@ -97,6 +100,18 @@ class movlen:
             else: 
                 i = i * -1
             self.cmy += i
+        #gets the diff for fulllengh
+        for i in self.fulllengh_nums:
+            if i > 0:
+                pass
+            elif i == 0:
+                pass
+            else: 
+                i = i * -1
+            self.full_len += i
+        self.x_nums.clear()
+        self.y_nums.clear()
+        self.fulllengh_nums.clear()
 
     def getscreensize(self):
         width, height= pyautogui.size()
@@ -105,8 +120,6 @@ class movlen:
     def autosave(self):
         while(True):
             self.savedata()
-            self.x_nums.clear()
-            self.y_nums.clear()
             time.sleep(300)
                 
     def main(self):
