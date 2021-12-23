0eb6d97f66c3018a6edc8527d553ebcf1bc00444
Pfefan <stefanpfeiffer04@gmail.com>
2021-12-23 18:20:57 +0100

    working on implementing a save system

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time, math, ctypes #importing standard libaries
+import time, math, ctypes, json #importing standard libaries
 import pyautogui, tkinter  #importing pyautogui for logging mouse position and tkinter for calculating the dpi (installed with pip install tk)
 from threading import Thread #importing threading libary to enalbe multiple threads
 
@@ -11,6 +11,7 @@ class movlen:
         self.cmx = 0
         self.cmy = 0    
         self.full_len = 0
+        self.save_data = {}
    
     def get_screensize(self):
         #https://stackoverflow.com/questions/54271887/calculate-screen-dpi
@@ -75,11 +76,17 @@ class movlen:
                 print(self.convert(self.cmy * self.pixelsize, "lengh moved on y Axes:"))
             elif i == "screensize":
                 print(self.getscreensize())
+            elif i == "load":
+                self.load_data()
             elif i == "help":
                 print("mov: calculates the distance you moved on your screen in cm")
                 print("screensize: prints the physical screen size in cm")
             else:
-                print(i, "is an unknown command")            
+                print(i, "is an unknown command")  
+
+    def getscreensize(self):
+        width, height= pyautogui.size()
+        return ("width: %.2f cm" % (self.pixelsize * width)) + ("\nheight: %.2f cm" % (self.pixelsize * height))          
 
     def convert(self, val, parameter):
         #Converts cm in to m and km
@@ -133,13 +140,33 @@ class movlen:
         self.y_nums.clear()
         self.fulllengh_nums.clear()
 
-    def getscreensize(self):
-        width, height= pyautogui.size()
-        return ("width: %.2f cm" % (self.pixelsize * width)) + ("\nheight: %.2f cm" % (self.pixelsize * height))
+        #saves the x, y and full_lengh variables to a file
+        self.save_data["pixel_x"] = self.cmx
+        self.save_data["pixel_y"] = self.cmy
+        self.save_data["pixel_full_lengh"] = self.full_len
+
+        with open('data.json', 'w+') as outfile:
+            json.dump(self.save_data, outfile)
+    
+    def load_data(self):        
+        try:
+            with open ('data.json', 'r+') as jsonfile:
+                tempkeys = json.load(jsonfile)
+                if(tempkeys == None) or (tempkeys == {}):
+                    pass
+                else:
+                    self.save_data = tempkeys
+                    print(self.save_data[0])
+                    self.cmy = self.save_data[1]
+                    self.full_len = self.save_data[2]
+        except:
+            with open('data.json', 'w+') as save:
+                json.dump(self.save_data, save)
+
 
     def autosave(self):
         while(True):
-            self.savedata()
+            #self.savedata()
             time.sleep(300)
                 
     def main(self):
